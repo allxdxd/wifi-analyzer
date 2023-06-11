f84d2d5378d13c68e24d49275d20b739838abac4
allxdxd <asdfnestor@gmail.com>
2023-06-11 17:37:35 -0500

    modified:   .gitignore 	modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from NetworksAPI import Networks
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # initialize class
 nt = Networks()
-
 # create a dataframe
 netDataframe = nt.convert_Dataframe()
-print(netDataframe)
 
+print(netDataframe)
