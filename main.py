7b7f828c4739083204fa2c564795f087f4232f37
allxdxd <asdfnestor@gmail.com>
2023-06-13 16:04:03 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from NetworksAPI import Networks
-import pandas as pd
-import matplotlib.pyplot as plt
+from datetime import datetime
 
+print('Realizando captura de redes...')
 # initialize class
 nt = Networks()
+
+print('Convirtiendo en Dataframe...\n\n')
 # create a dataframe
 netDataframe = nt.convert_Dataframe()
 
-print(netDataframe)
+print('Mostrando resultados\n\n')
+# showing
+date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+print(date)
+print(netDataframe[['SSID','BSSID','Authentication', 'Encryption', 'dBm','Channel']])
+
+# others things
+print('\n\nEliga el número de una opción: ')
+print("""
+1: Exportar como archivo Excel
+2: Exportar como archivo CSV
+
+# Cualquier otra tecla cerrará el proceso
+""")
+try: 
+    op = int(input('\nOpción: '))
+    name = f'capture.{datetime.now().strftime("%d.%m.%Y.%H.%M")}'
+    if op == 1:
+        print('Convirtiendo a .xlsx')
+        netDataframe.to_excel(f"{name}.xlsx")
+    if op == 2:
+        print('Convirtiendo a .csv')
+        netDataframe.to_csv(f"./{name}.csv")
+except:
+    print('Cerrando ...')
+
